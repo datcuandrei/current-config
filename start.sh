e17852f4127e7bb5005fe46e064da6cedeb8613d
Andrei D <49794799+datcuandrei@users.noreply.github.com>
2021-03-03 15:28:53 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 sudo su <<HERE
 sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
 sudo chmod a+rw /sys/class/leds/smc::kbd_backlight/brightness
-echo 650 > /sys/class/backlight/intel_backlight/brightness
+echo 500 > /sys/class/backlight/intel_backlight/brightness
 echo 20 > /sys/class/leds/smc::kbd_backlight/brightness
 cd Documents/v2.3.1 && sh maclinuxutils.sh
 HERE
